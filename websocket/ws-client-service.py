f85fdb3f8a97ad2f4ea2c1f17dc18be86fb1cb0d
abdullah sirajeldeen <abdullahsirajeldeen@gmail.com>
2024-06-20 13:59:49 +0000

    refactor

--- websocket/ws-client-service.py
+++ websocket/ws-client-service.py
@@ -11,6 +11,7 @@ ALGORITHM = "HS256"
 
 app = FastAPI()
 
+
 async def test_websocket(token: str):
     uri = f"ws://localhost:8000/ws/{token}"
 
@@ -24,11 +25,13 @@ async def test_websocket(token: str):
             finish_time = datetime.now().strftime("%H:%M:%S")
             print(f"Connection closed. Start time: {start_time}, Finish time: {finish_time}")
 
+
 @app.get("/start-consumer")
 async def start_consumer():
     token = create_jwt_token()
     asyncio.create_task(test_websocket(token))
     return {"message": "WebSocket consumer started"}
+
 
 def create_jwt_token():
     payload = {
@@ -38,6 +41,8 @@ def create_jwt_token():
     }
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8001)
